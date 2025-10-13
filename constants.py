7c71a042076a21790b38f2aa757c0daed45572be
David-Chan-Ho2 <dho12@charlotte.edu>
2025-10-13 09:13:39 -0400

    add constants

--- constants.py
+++ constants.py
@@ -12,3 +12,21 @@ DROPLET_DENSITY = 1000
 
 # Printer resolution in dots per inch [dpi]
 PRINTER_RESOLUTION = 300
+
+# Distance between the droplet gun and the paper in millimeters [m^-3]
+DROPLET_GUN_DISTANCE = 3E-3
+
+# Width of the capacitor in milimeters [m^-3]
+CAPACITOR_WIDTH = 1E-3
+
+# Length of the capacitor in milimeters [m^3]
+CAPACITOR_LENGTH = 0.5E-3
+
+# Distance between the capacitor and the paper in milimeters [m^3]
+CAPACITOR_DISTANCE = 1.25E-3
+
+# Height of the paper in inches [in]
+PAPER_HEIGHT = 11
+
+# Width of the paper in inches [in]
+PAPER_WIDTH = 8.5
