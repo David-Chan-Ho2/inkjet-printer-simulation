d989d4886d4d2ed94477d6794845f0151f5a60af
David-Chan-Ho2 <dho12@charlotte.edu>
2025-10-13 09:02:33 -0400

    add starter files

---
+++ constants.py
@@ -0,0 +1,14 @@
+# Diameter of the droplet in meters [m]
+DROPLET_DIAMETER = 84E-6 
+
+# Charge of the droplet in Coulombs [C]
+DROPLET_CHARGE = -1.9E-10  
+
+# Velocity of the droplet in meters per second [m/s]
+DROPLET_VELOCITY = 20
+
+# Density of the droplet in kilogram per meter cubed [kg/m^3]
+DROPLET_DENSITY = 1000
+
+# Printer resolution in dots per inch [dpi]
+PRINTER_RESOLUTION = 300
